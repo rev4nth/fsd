a1b57288bb5fd06df35ffbd8d21956226e106ce2
rev4nth <163738353+rev4nth@users.noreply.github.com>
2025-05-11 07:04:21 +0530

    Update BuyerPropertiesPage.jsx

--- BACKEND/FRONTEND/PROPERTYMANAGEMENTSYSTEM/src/pages/BuyerPropertiesPage.jsx
+++ BACKEND/FRONTEND/PROPERTYMANAGEMENTSYSTEM/src/pages/BuyerPropertiesPage.jsx
@@ -24,7 +24,7 @@ const BuyerPropertiesPage = () => {
 
   const fetchProperties = async () => {
     try {
-      const response = await axios.get('http://https://property-i3v1.onrender.com:2509/api/public/properties');
+      const response = await axios.get('https://fsd-jz2r.onrender.com/api/public/properties');
       setProperties(response.data);
       setLoading(false);
     } catch (error) {
@@ -50,7 +50,7 @@ const BuyerPropertiesPage = () => {
     try {
       const token = localStorage.getItem('token');
       const response = await axios.post(
-        'http://https://property-i3v1.onrender.com:2509/api/bookings',
+        'https://fsd-jz2r.onrender.com/api/bookings',
         {
           propertyId: selectedProperty.id,
           visitDate: bookingData.visitDate,
